9c3c3e39bc38f420cb99243ebc10ee84df20fec2
QA <talha.naeem@volopa.com>
2025-09-02 21:14:05 +0500

    open PR 3

--- cypress/e2e/Signup.cy.js
+++ cypress/e2e/Signup.cy.js
@@ -20,4 +20,7 @@ describe('template spec', () => {
   it('passed', () => {
     cy.visit('https://example.cypress.io')
   })
+  it('passed', () => {
+    cy.visit('https://example.cypress.io')
+  })
 })
